0a26cd8e6ff83c78741f48169c064625008878b9
Jason Yu <jason_yu@executivecentre.com>
2019-07-05 16:27:54 +0800

    Update AspNetCore SignalR

--- SignalR_Client/wwwroot/js/site.js
+++ SignalR_Client/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:11978/SomeHub").build();
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:11978/SomeHub").build();
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
